ec2408a45d6ee0738826815f83f57fa165d6efad
yueyuyuan <yue.yuyuan@upex.co>
2019-11-22 18:38:30 +0800

    1

--- setup.py
+++ setup.py
@@ -7,13 +7,18 @@ setup(
     author = "yuyuan yue",
     author_email = "last_yearv@163.com",
     description = "app store connect api cli user apikey and issuer_id to authorize",
-    license = "MIT",
     url = "https://github.com/agony5/connectcli",
     install_requires=[
         'Click',
         'requests',
         'authlib'
     ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
     entry_points='''
         [console_scripts]
         connectcli=connectcli.main:cli
