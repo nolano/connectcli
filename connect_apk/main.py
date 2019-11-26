c6c666fc471ae08274e17e821c7777fd5087fc82
yueyuyuan <yue.yuyuan@upex.co>
2019-11-26 14:39:10 +0800

    cli 增加组命令

--- connect_apk/main.py
+++ connect_apk/main.py
@@ -9,163 +9,162 @@ from connectapi import ConnectApi
 import click
 
 @click.group()
-def cli():
+@click.option('-k','--apikey','apikey',metavar='',help='app store connect api key',required=True)
+@click.option('-i','--issuer','issuer_id',metavar='',help='app store connect issuer id',required=True)
+@click.option('-p','--privatekey_path','privatekey_path',type=click.Path(exists=True),metavar='',help='privatekey file path , default linux user root path ".private_keys"')
+@click.pass_context
+def cli(ctx, apikey, issuer_id,privatekey_path):
     '''
     app store connect api cli with python user apikey private_key and issuer_id to authorize
     
     how to get apikey,private_key and issuer_id ? https://developer.apple.com/documentation/appstoreconnectapi
     '''
+    
+    if privatekey_path:
+        if apikey in privatekey_path:
+            ctx.obj = ConnectApi(apikey,issuer_id,privatekey_path)
+        else:
+            connectool.raiseError('private key path error apikey '+apikey+' not in '+privatekey_path)
+    else:
+        ctx.obj = ConnectApi(apikey,issuer_id,privatekey_path)
+    
 
 @click.command()
-@click.option('-k','--apikey','apikey',metavar='',help='app store connect api key',required=True)
-@click.option('-i','--issuer','issuer_id',metavar='',help='app store connect issuer id',required=True)
 @click.option('-n','--name','name',metavar='',help='device name',required=True)
 @click.option('-u','--udid','udid',metavar='',help='device udid',required=True)
-def registerdevice(apikey,issuer_id,name,udid):
+@click.pass_obj
+def registerdevice(api,name,udid):
     '''register device into app store connect with name and udid '''
-    result = ConnectApi(apikey,issuer_id).register_device(name,udid)
+    result = api.register_device(name,udid)
     click.echo(json.dumps(result))
     return result
 
 @click.command()
-@click.option('-k','--apikey','apikey',metavar='',help='app store connect api key',required=True)
-@click.option('-i','--issuer','issuer_id',metavar='',help='app store connect issuer id',required=True)
 @click.option('-l','--limit','limit',default=100,metavar='', help='the devices count,default 100')
 @click.option('-s','--sort','sort',default='id',metavar='',help='the devices sort,default id')
-def devices(apikey,issuer_id,limit,sort):
+@click.pass_obj
+def devices(api,limit,sort):
     '''list devices with limit and sort '''
-    result = ConnectApi(apikey,issuer_id).list_devices(limit,sort)
+    result = api.list_devices(limit,sort)
     click.echo(json.dumps(result))
     return result
 
 @click.command()
-@click.option('-k','--apikey','apikey',metavar='',help='app store connect api key',required=True)
-@click.option('-i','--issuer','issuer_id',metavar='',help='app store connect issuer id',required=True)
 @click.option('-c','--csr_path','csr_path',type=click.Path(exists=True),metavar='',help='certificare csr request file path,must value',required=True)
 @click.option('-o','--out','out_path',type=click.Path(exists=True),default=lambda: os.environ.get('HOME', ''),metavar='',help='certificate file out path , default user root path')
 @click.option('-t','--type','type',type=click.Choice(['IOS_DEVELOPMENT','IOS_DISTRIBUTION']),default='IOS_DEVELOPMENT',metavar='',help='the certificate type,choice:[IOS_DEVELOPMENT,IOS_DISTRIBUTION] default IOS_DEVELOPMENT')
-def registercertificate(apikey,issuer_id,csr_path,out_path,type):
+@click.pass_obj
+def registercertificate(api,csr_path,out_path,type):
     '''register certificate with csr_path,out_path and type'''
-    result = ConnectApi(apikey,issuer_id).register_certificate(csr_path=csr_path,out_path=out_path,type=type)
+    result = api.register_certificate(csr_path=csr_path,out_path=out_path,type=type)
     click.echo(json.dumps(result))
     return result
 
 @click.command()
-@click.option('-k','--apikey','apikey',metavar='',help='app store connect api key',required=True)
-@click.option('-i','--issuer','issuer_id',metavar='',help='app store connect issuer id',required=True)
 @click.option('-i','--id','id',metavar='', help='the certificate id',required=True)
-def deletecertificate(apikey,issuer_id,id):
+@click.pass_obj
+def deletecertificate(api,id):
     '''delete certificate with id '''
-    result = ConnectApi(apikey,issuer_id).delete_certificate(id)
+    result = api.delete_certificate(id)
     click.echo(json.dumps(result))
     return result
 
 @click.command()
-@click.option('-k','--apikey','apikey',metavar='',help='app store connect api key',required=True)
-@click.option('-i','--issuer','issuer_id',metavar='',help='app store connect issuer id',required=True)
 @click.option('-l','--limit','limit',default=100,metavar='', help='the certificates count,default 100')
 @click.option('-s','--sort','sort',default='id',metavar='',help='the certificates sort,default id')
-def certificates(apikey,issuer_id,limit,sort):
+@click.pass_obj
+def certificates(api,limit,sort):
     '''list certificate with limit and sort '''
-    result = ConnectApi(apikey,issuer_id).list_certificates(limit,sort)
+    result = api.list_certificates(limit,sort)
     click.echo(json.dumps(result))
     return result
 
 @click.command()
-@click.option('-k','--apikey','apikey',metavar='',help='app store connect api key',required=True)
-@click.option('-i','--issuer','issuer_id',metavar='',help='app store connect issuer id',required=True)
 @click.option('-b','--bundleid','bundleid',metavar='',help='bundle id',required=True)
 @click.option('-t','--teamid','teamid',metavar='',help='team id',required=True)
 @click.option('-n','--name','name',metavar='',help='bunle id name',required=True)
-def registerbundleid(apikey,issuer_id,bundleid,teamid,name):
+@click.pass_obj
+def registerbundleid(api,bundleid,teamid,name):
     '''register bundle id into app store connect with bundle_id team_id and name '''
-    result = ConnectApi(apikey,issuer_id).register_bundle_id(bundleid,teamid,name)
+    result = api.register_bundle_id(bundleid,teamid,name)
     click.echo(json.dumps(result))
     return result
 
 @click.command()
-@click.option('-k','--apikey','apikey',metavar='',help='app store connect api key',required=True)
-@click.option('-i','--issuer','issuer_id',metavar='',help='app store connect issuer id',required=True)
 @click.option('-l','--limit','limit',default=100,metavar='', help='the bundleid count,default 100')
 @click.option('-s','--sort','sort',default='id',metavar='',help='the bundleid sort,default id')
-def bundleids(apikey,issuer_id,limit,sort):
+@click.pass_obj
+def bundleids(api,limit,sort):
     '''list bundleids with limit and sort '''
-    result = ConnectApi(apikey,issuer_id).list_bundle_ids(limit,sort)
+    result = api.list_bundle_ids(limit,sort)
     click.echo(json.dumps(result))
     return result
 
 @click.command()
-@click.option('-k','--apikey','apikey',metavar='',help='app store connect api key',required=True)
-@click.option('-i','--issuer','issuer_id',metavar='',help='app store connect issuer id',required=True)
 @click.option('-i','--id','id',metavar='', help='the bundleid id',required=True)
-def deletebundleid(apikey,issuer_id,id):
+@click.pass_obj
+def deletebundleid(api,id):
     '''delete bundleid with id '''
-    result = ConnectApi(apikey,issuer_id).delete_bundle_id(id)
+    result = api.delete_bundle_id(id)
     click.echo(json.dumps(result))
     return result
 
 @click.command()
-@click.option('-k','--apikey','apikey',metavar='',help='app store connect api key',required=True)
-@click.option('-i','--issuer','issuer_id',metavar='',help='app store connect issuer id',required=True)
 @click.option('-i','--id','id',metavar='', help='the bundleid id',required=True)
-def getbundleid(apikey,issuer_id,id):
+@click.pass_obj
+def getbundleid(api,id):
     '''get bundleid with id '''
-    result = ConnectApi(apikey,issuer_id).get_bundle_id(id)
+    result = api.get_bundle_id(id)
     click.echo(json.dumps(result))
     return result
 
 @click.command()
-@click.option('-k','--apikey','apikey',metavar='',help='app store connect api key',required=True)
-@click.option('-i','--issuer','issuer_id',metavar='',help='app store connect issuer id',required=True)
 @click.option('-i','--id','id',metavar='', help='the bundleid id',required=True)
-def getbundleidprofiles(apikey,issuer_id,id):
+@click.pass_obj
+def getbundleidprofiles(api,id):
     '''get bundleid profiles with id '''
-    result = ConnectApi(apikey,issuer_id).get_bundle_id_profiles(id)
+    result = api.get_bundle_id_profiles(id)
     click.echo(json.dumps(result))
     return result
 
 @click.command()
-@click.option('-k','--apikey','apikey',metavar='',help='app store connect api key',required=True)
-@click.option('-i','--issuer','issuer_id',metavar='',help='app store connect issuer id',required=True)
 @click.option('-n','--name','name',metavar='',help='profile name',required=True)
 @click.option('-b','--bundleid','bundleid',metavar='',help='profile contact bundleid id',required=True)
 @click.option('-c','--certificate','certificate',metavar='',help='profile contact certificate id',required=True)
 @click.option('-o','--out','out_path',type=click.Path(exists=True),default=lambda: os.environ.get('HOME', ''),metavar='',help='profile file out path , default user root path')
 @click.option('-t','--type','type',type=click.Choice(['IOS_APP_DEVELOPMENT','IOS_APP_STORE','IOS_APP_ADHOC']),default='IOS_DEVELOPMENT',metavar='',help='the certificate type,choice:[IOS_APP_DEVELOPMENT, IOS_APP_STORE, IOS_APP_ADHOC] default IOS_APP_ADHOC')
-def createprofile(apikey,issuer_id,name,bundleid,certificateid,out_path,type):
+@click.pass_obj
+def createprofile(api,name,bundleid,certificateid,out_path,type):
     '''create profile with name,bundleid,certificateid,out_path and type'''
-    result = ConnectApi(apikey,issuer_id).create_profile(name=name,bundle_id=bundleid,certificate_id=certificateid,out_path=out_path,type=type)
+    result = api.create_profile(name=name,bundle_id=bundleid,certificate_id=certificateid,out_path=out_path,type=type)
     click.echo(json.dumps(result))
     return result
 
 @click.command()
-@click.option('-k','--apikey','apikey',metavar='',help='app store connect api key',required=True)
-@click.option('-i','--issuer','issuer_id',metavar='',help='app store connect issuer id',required=True)
 @click.option('-i','--id','id',metavar='', help='the profile id',required=True)
-def deleteprofile(apikey,issuer_id,id):
+@click.pass_obj
+def deleteprofile(api,id):
     '''delete profile with id '''
-    result = ConnectApi(apikey,issuer_id).delete_profile(id)
+    result = api.delete_profile(id)
     click.echo(json.dumps(result))
     return result
 
 @click.command()
-@click.option('-k','--apikey','apikey',metavar='',help='app store connect api key',required=True)
-@click.option('-i','--issuer','issuer_id',metavar='',help='app store connect issuer id',required=True)
 @click.option('-l','--limit','limit',default=100,metavar='', help='the profiles count,default 100')
 @click.option('-s','--sort','sort',default='id',metavar='',help='the profiles sort,default id')
-def profiles(apikey,issuer_id,limit,sort):
+@click.pass_obj
+def profiles(api,limit,sort):
     '''list profiles with limit and sort '''
-    result = ConnectApi(apikey,issuer_id).list_profiles(limit,sort)
+    result = api.list_profiles(limit,sort)
     click.echo(json.dumps(result))
     return result
 
 @click.command()
-@click.option('-k','--apikey','apikey',metavar='',help='app store connect api key',required=True)
-@click.option('-i','--issuer','issuer_id',metavar='',help='app store connect issuer id',required=True)
 @click.option('-i','--id','id',metavar='', help='the profile id',required=True)
-def requestprofile(apikey,issuer_id,id):
+@click.pass_obj
+def requestprofile(api,id):
     '''request profile with id '''
-    result = ConnectApi(apikey,issuer_id).request_profile(id)
+    result = api.request_profile(id)
     click.echo(json.dumps(result))
     return result
 
